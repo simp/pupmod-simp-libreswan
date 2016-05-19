67b6b80c79fbbb57c022e1676c6b9bd0e58b3b17
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2016-05-19 21:08:43 +0000

    (SIMP-1125) Add IPSEC Functionality
    New module to add IPSEC functionality for
    SIMP V5.1 and later.
    
    Name updated to pupmod-simp-libreswan completed.
    
    SIMP-1125 #close

---
+++ manifests/config/logging.pp
@@ -0,0 +1,10 @@
+# == Class libreswan::config::logging
+#
+# This class is meant to be called from ipsec.
+# It ensures that logging rules are defined.
+#
+class libreswan::config::logging {
+  assert_private()
+
+}
+
