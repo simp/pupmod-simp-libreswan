7c469136198fc742772f188a596d561a09fb0ad8
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2016-05-19 21:08:43 +0000

    (SIMP-719) Add IPSEC Functionality
    New module to add IPSEC functionality.
    
    SIMP-719 #comment first commit for peer review
    SIMP-719 #close
    
    Change-Id I730c8b7f99a23d6e41408516d9b8ce70ef3c1cf9

---
+++ manifests/config/pki.pp
@@ -0,0 +1,21 @@
+
+# Ensures that there are PKI certificates readable by the ipsec user in
+#
+class ipsec::config::pki {
+  assert_private()
+
+  file { $::ipsec::certsource:
+    ensure =>  directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700'
+  }
+  if $::ipsec::use_simp_pki {
+    include '::pki'
+    ::pki::copy { $::ipsec::certsource:
+        notify  => Class['ipsec::nsspki'],
+        require => File["$::ipsec::certsource"]
+    }
+  }
+}
+
