b208916ae4b969f3b6d8f8fd4eb41c6b88535746
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2016-06-30 14:22:27 +0000

    (SIMP-1125) Libreswan-IPSEC Module for v5
    This includes the fixes and tests for integrating
    the libreswan module into version 5.1 of SIMP.
    
    SIMP-1125 #close
    Change-Id:  I99024bfb0dc65ebd2569e9640a51f56d2e06a758

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+describe 'libreswan::config' do
+let(:pre_condition) { 'class { "libreswan": service_name => "ipsec",}'}
+let(:facts) { {:osfamily => 'RedHat'}}
+  context "libreswan class without any parameters" do
+    it { is_expected.to create_file('/etc/ipsec.conf').with({
+      :owner   => 'root',
+      :mode    => '0400',
+      :notify  => 'Service[ipsec]',
+      })
+    }
+    it { is_expected.to create_file('/etc/ipsec.secrets').with({
+      :owner   => 'root',
+      :mode    => '0400',
+      })
+    }
+  end
+  context "libreswan::config should initialize NSS database" do
+    it { is_expected.to create_libreswan__nss__init_db('NSSDB /etc/ipsec.d').with({
+      :require  => 'File[/etc/ipsec.conf]',
+      :notify   => "Class[Libreswan::Nsspki]"
+      })
+    }
+  end
+end
