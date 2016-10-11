d48c97ad2eec21e179c4a262c5fcc81286634343
Liz Nemsick <lnemsick.simp@gmail.com>
2016-10-11 13:56:41 -0400

    (SIMP-1671) Fix ipsec config bugs and enhance
    - Fixed several bugs in the following categories
      - Use of wrong variables (cut and paste errors or typos)
      - Improper parameter validations
      - Parameters not being used in erb files.
      - OBE names of ipsec settings.
      - Missing quotes/braces around settings that can contain whitespace
    - Exposed more ipsec settings.
    - Allow IPv6 addresses
    
    SIMP-1671 #comment  1 of 2
    
    Change-Id: I43569030a337f04b8bdc5a2c172268e2908afc11

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -22,7 +22,7 @@ describe 'libreswan' do
 
         context "libreswan class with firewall enabled" do
           let(:params) {{
-            :client_nets     => ['all'],
+            :ipsec_client_nets     => ['192.168.0.0/16'],
             :ikeport => '50',
             :nat_ikeport => '4500',
             :simp_firewall => true,
@@ -46,12 +46,229 @@ describe 'libreswan' do
           it { is_expected.to_not contain_class('haveged') }
         end
 
-        context 'with invalid input' do
-          let(:params) {{:use_haveged => 'invalid_input'}}
-          it 'with use_haveged as a string' do
-            expect {
-              is_expected.to compile
-            }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/invalid_input" is not a boolean/)
+        #TODO flesh out full list of validation successes and failures,
+
+        describe 'accept valid parameter options' do
+          describe 'ddos-mode enum' do
+            [ 'busy','unlimited', 'auto'].each do |valid_enum|
+              context "ddos-mod #{valid_enum}" do
+                let(:params) {{:ddos_mode => valid_enum}}
+                it { is_expected.to compile}
+              end
+            end
+          end
+
+          describe 'protostack enum' do
+            [ 'netkey','klips', 'mast'].each do |valid_enum|
+              context "protostack #{valid_enum}" do
+                let(:params) {{:protostack => valid_enum}}
+                it { is_expected.to compile}
+              end
+            end
+          end
+
+          describe 'interfaces enum' do
+            [ ['%none'], ['%defaultroute'], ['ipsec0=eth1', 'ipsec1=ppp0'] ].each do |valid_enum|
+              context "protostack #{valid_enum}" do
+                let(:params) {{:interfaces => valid_enum}}
+                it { is_expected.to compile}
+              end
+            end
+          end
+        end
+
+        describe 'with invalid yes/no parameters' do
+          [ :strictcrlpolicy,
+            :hidetos,
+            :plutofork,
+            :uniqueids,
+            :plutorestartoncrash,
+            :fragicmp,
+            :perpeerlog,
+            :ocsp_enable,
+            :ocsp_strict,
+            :logappend,
+            :logtime
+          ].each do |yesno_param|
+            context "invalid #{yesno_param}" do
+              let(:params) {{yesno_param => 'false'}}
+              it 'fails to compile' do
+                expect {
+                  is_expected.to compile
+                }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'false' is not 'yes' or 'no'/)
+              end
+            end
+          end
+        end
+
+        describe 'with invalid integer parameters' do
+          [ :nhelpers,
+            :overridemtu,
+            :keep_alive,
+            :crlcheckinterval,
+            :ocsp_timeout,
+            :ddos_ike_treshold,
+            :ikeport,
+            :nflog_all,
+            :nat_ikeport
+          ].each do |int_param|
+            context "invalid #{int_param}" do
+              let(:params) {{int_param => 'not-a-number'}}
+              it 'fails to compile' do
+                expect {
+                  is_expected.to compile
+                }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'\["not-a-number"\]' is not an integer/)
+              end
+            end
+          end
+        end
+
+        describe 'with invalid bool parameters' do
+          [ :simp_firewall,
+            :use_simp_pki,
+            :use_fips,
+            :use_haveged
+          ].each do |bool_param|
+            context "invalid #{bool_param}" do
+              let(:params) {{bool_param => 'FALSE'}}
+              it 'fails to compile' do
+                expect {
+                  is_expected.to compile
+                }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/FALSE" is not a boolean/)
+              end
+            end
+          end
+        end
+
+        describe 'with invalid absolute path parameters' do
+          [ :perpeerlogdir,
+            :logfile,
+            :ipsecdir,
+            :secretsfile,
+            :dumpdir,
+            :certsource,
+            :pkiroot
+          ].each do |abs_path_param|
+            context "invalid #{abs_path_param}" do
+              let(:params) {{abs_path_param => './myfile'}}
+              it 'fails to compile' do
+                expect {
+                  is_expected.to compile
+                }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/".\/myfile" is not an absolute path/)
+              end
+            end
+          end
+        end
+
+        describe 'with invalid string parameters' do
+          [ :service_name,
+            :package_name,
+            :myid,
+            :listen,
+            :myvendorid,
+            :statsbin,
+            :ocsp_uri,
+            :ocsp_trustname,
+            :syslog,
+            :klipsdebug,
+            :plutodebug
+          ].each do |string_param|
+            context "invalid #{string_param}" do
+              let(:params) {{string_param => ['string-in-an-array']}}
+              it 'fails to compile' do
+                expect {
+                  is_expected.to compile
+                }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/\["string-in-an-array"\] is not a string/)
+              end
+            end
+          end
+        end
+
+        describe 'with invalid parameters' do
+          context 'invalid ipsec_client_nets' do
+#TODO renable this example once simplib's validate_net_list is fixed!
+#            let(:params) {{:ipsec_client_nets => ['1.2.3.4/33']}}
+            let(:params) {{:ipsec_client_nets => ['1.2..4/32']}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'1.2..4\/32' is not a valid network/)
+            end
+          end
+
+          context 'invalid virtual_private' do
+            let(:params) {{:virtual_private => '1.2.3.0/24'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/"1.2.3.0\/24" is not an Array/)
+            end
+          end
+
+          context 'invalid ocsp_uri' do
+            let(:params) {{:ocsp_uri => ':myuri'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/':myuri' is not a valid URI/)
+            end
+          end
+
+          context 'invalid ddos_mode' do
+            let(:params) {{:ddos_mode => 'none'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'busy,unlimited,auto' does not contain 'none'/)
+            end
+          end
+
+          context 'invalid protostack' do
+            let(:params) {{:protostack => 'none'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'netkey,klips,mast' does not contain 'none'/)
+            end
+          end
+
+          context 'malformed listen' do
+            let(:params) {{:listen => '1..2.3'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'1..2.3' is not a valid network/)
+            end
+          end
+
+#TODO enable once code validates this
+=begin
+          context 'invalid listen' do
+            let(:params) {{:listen => '1.2.3.0/24'}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/'1..2.3' is not a valid network/)
+            end
+          end
+=end
+
+          context 'invalid interfaces' do
+            let(:params) {{:interfaces => ['eth1=']}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/"eth1=" does not match/)
+            end
+          end
+
+          context "with invalid virtual_private " do
+            let(:params) {{:virtual_private     => ['267.2.3.0/24']}}
+            it 'fails to compile' do
+              expect {
+                is_expected.to compile
+              }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/267.2.3.0\/24 in virtual_private is not an IPv4\/IPv6 address/)
+            end
           end
         end
       end
