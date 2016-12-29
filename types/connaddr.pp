929da100e73b83ae9afefe1197eb10376cfaa786
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2016-12-29 13:22:51 -0500

    (SIMP-2288) Strong type - libreswan
      fixed spacing errors
    
    SIMP-2339 #close

--- types/connaddr.pp
+++ types/connaddr.pp
@@ -1,9 +1,1 @@
-type Libreswan::ConnAddr = Variant[
-                             Enum['%any','%defaultroute',
-                                  '%opportunistic',
-                                  '%opportunisticgroup',
-                                  '%group'],
-                             Simplib::IP::V4,
-                             Simplib::IP::V6,
-                             Pattern['^%[a-zA-Z]+\d+$']
-                          ]
+type Libreswan::ConnAddr = Variant[ Enum['%any','%defaultroute', '%opportunistic', '%opportunisticgroup', '%group'], Simplib::IP::V4, Simplib::IP::V6, Pattern['^%[a-zA-Z]+\d+$'] ]
