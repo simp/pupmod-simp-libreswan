a0a4193d256210af981da751c12e6790712bb6d1
jeckles <joeleckles@gmail.com>
2019-08-19 14:41:57 -0400

    Reverting acceptance tests to original format. Deleted tests for version 6 OS.

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -25,7 +25,6 @@ def get_public_network_interface(host)
     ifc.include?('ip_') # IPsec tunnel
   end
   fail("Could not determine the interface for the #{host}'s public network") unless interfaces.size >= 1
-  interfaces.sort!
   interfaces.sort[0]
 end
 
@@ -122,14 +121,16 @@ simp_options::pki::source: '/etc/pki/simp-testing/pki'
 EOM
     }
     let(:testfile) { testfile = "/tmp/testfile.#{Time.now.to_i}" }
-    let(:nc) {'/bin/nc'}
+    let(:nc) { # we want the full path so we can pkill intelligently
+        '/bin/nc'
+    }
 
     context 'test prep' do
       it 'should install haveged, nmap-ncat, and tcpdump' do
         [left, right].flatten.each do |node|
            # Generate ALL of the entropy .
-           apply_manifest_on(node, haveged, :catch_failures => true)    
-           node.install_package('nmap-ncat')
+           apply_manifest_on(node, haveged, :catch_failures => true)
+             node.install_package('nmap-ncat')
         end
         left.install_package('tcpdump')
       end
@@ -208,154 +209,154 @@ EOM
         end
       end
 
-    #  context 'when tunnel goes down' do
-    #    it 'should detect disabled tunnel' do
-    #      on left, 'puppet resource service ipsec ensure=stopped', :acceptable_exit_codes => [0]
-    #      wait_for_command_success(left, "ipsec status |& grep 'Pluto is not running'")
+      context 'when tunnel goes down' do
+        it 'should detect disabled tunnel' do
+          on left, 'puppet resource service ipsec ensure=stopped', :acceptable_exit_codes => [0]
+          wait_for_command_success(left, "ipsec status |& grep 'Pluto is not running'")
 
           # can take up to 2 minutes for right to timeout tunnel, so restart instead to detect
           # failure immediately
-    #     wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 0\"")
-    #      wait_for_command_success(right, "ip xfrm policy | grep 'mode transport'")
-    #    end
+          wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 0\"")
+          wait_for_command_success(right, "ip xfrm policy | grep 'mode transport'")
+        end
 
-    #    it "should drop data because of broken tunnel" do
+        it "should drop data because of broken tunnel" do
           # try to send TCP data from right to left
-    #      on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
-    #      sleep(2)
-    #      on right, "echo 'this is a test of a downed tunnel without firewall' | #{nc} -s #{rightip} #{left} #{nc_port} ",
-    #        :acceptable_exit_codes => [1]
+          on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
+          sleep(2)
+          on right, "echo 'this is a test of a downed tunnel without firewall' | #{nc} -s #{rightip} #{left} #{nc_port} ",
+            :acceptable_exit_codes => [1]
 
-    #        verify data does NOT reach left
-    #      on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
-    #        expect(stdout).to_not match(/this is a test of a downed tunnel without firewall/)
-    #      end
-    #      on left, "pkill -f '#{nc} -l #{nc_port}'", :acceptable_exit_codes => [0]
-    #    end
-    #  end
+          # verify data does NOT reach left
+          on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
+            expect(stdout).to_not match(/this is a test of a downed tunnel without firewall/)
+          end
+          on left, "pkill -f '#{nc} -l #{nc_port}'", :acceptable_exit_codes => [0]
+        end
+      end
 
-    #  context 'when connection with firewall' do
-    #    let(:client_net) {
-    #      quads = leftip.split('.')  # doesn't matter if use left or right ip
-    #      quads[2] = '0'
-    #      quads[3] = '0'
-    #      quads.join('.') + '/16'
-    #    }
-    #    let(:hieradata_with_firewall_left)  {
-    #      hieradata_left +
-    #      "simp_options::firewall: yes\n" +
-    #      "simp_options::trusted_nets : ['#{client_net}']\n"
-    #    }
-    #    let(:hieradata_with_firewall_right) {
-    #      hieradata_right +
-    #      "simp_options::firewall: yes\n" +
-    #      "simp_options::trusted_nets : ['#{client_net}']\n"
-    #    }
-    #    let(:leftconnection_with_firewall) { leftconnection +
-    #      "      class { 'iptables': }\n" +
-    #      "      iptables::rule {'allow_public_network_interface':\n" +
-    #      "        content => '-A LOCAL-INPUT -i #{get_public_network_interface(left)} -j ACCEPT',\n" +
-    #      "        apply_to => 'all',\n" +
-    #      "        order => 11\n" +
-    #      "      }\n" +
-    #      "      iptables::rule {'allow_decrypted_nc_traffic':\n" +
-    #      "        content => '-A LOCAL-INPUT -p tcp --dport #{nc_port} -j ACCEPT',\n" +
-    #      "        apply_to => 'all',\n" +
-    #      "        order => 11\n" +
-    #      "      }\n" +
-    #      "      iptables::listen::tcp_stateful { 'allow_sshd':\n" +
-    #      "        order => 8,\n" +
-    #      "        trusted_nets => ['ALL'],\n" +
-    #      "        dports => 22,\n" +
-    #      "      }\n"
-    #    }
-    #    let(:rightconnection_with_firewall) { rightconnection +
-    #      "      class { 'iptables': }\n" +
-    #      "      iptables::rule {'allow_public_network_interface':\n" +
-    #      "        content => '-A LOCAL-INPUT -i #{get_public_network_interface(right)} -j ACCEPT',\n" +
-    #      "        apply_to => 'all',\n" +
-    #      "        order => 11\n" +
-    #      "      }\n" +
-    #      "      iptables::rule {'allow_decrypted_nc_traffic':\n" +
-    #      "        content => '-A LOCAL-INPUT -p tcp --dport #{nc_port} -j ACCEPT',\n" +
-    #      "        apply_to => 'all',\n" +
-    #      "        order => 11\n" +
-    #      "      }\n" +
-    #      "      iptables::listen::tcp_stateful { 'allow_sshd':\n" +
-    #      "        order => 8,\n" +
-    #      "        trusted_nets => ['ALL'],\n" +
-    #      "        dports => 22,\n" +
-    #      "      }\n"
-    #    }
+      context 'when connection with firewall' do
+        let(:client_net) {
+          quads = leftip.split('.')  # doesn't matter if use left or right ip
+          quads[2] = '0'
+          quads[3] = '0'
+          quads.join('.') + '/16'
+        }
+        let(:hieradata_with_firewall_left)  {
+          hieradata_left +
+          "simp_options::firewall: yes\n" +
+          "simp_options::trusted_nets : ['#{client_net}']\n"
+        }
+        let(:hieradata_with_firewall_right) {
+          hieradata_right +
+          "simp_options::firewall: yes\n" +
+          "simp_options::trusted_nets : ['#{client_net}']\n"
+        }
+        let(:leftconnection_with_firewall) { leftconnection +
+          "      class { 'iptables': }\n" +
+          "      iptables::rule {'allow_public_network_interface':\n" +
+          "        content => '-A LOCAL-INPUT -i #{get_public_network_interface(left)} -j ACCEPT',\n" +
+          "        apply_to => 'all',\n" +
+          "        order => 11\n" +
+          "      }\n" +
+          "      iptables::rule {'allow_decrypted_nc_traffic':\n" +
+          "        content => '-A LOCAL-INPUT -p tcp --dport #{nc_port} -j ACCEPT',\n" +
+          "        apply_to => 'all',\n" +
+          "        order => 11\n" +
+          "      }\n" +
+          "      iptables::listen::tcp_stateful { 'allow_sshd':\n" +
+          "        order => 8,\n" +
+          "        trusted_nets => ['ALL'],\n" +
+          "        dports => 22,\n" +
+          "      }\n"
+        }
+        let(:rightconnection_with_firewall) { rightconnection +
+          "      class { 'iptables': }\n" +
+          "      iptables::rule {'allow_public_network_interface':\n" +
+          "        content => '-A LOCAL-INPUT -i #{get_public_network_interface(right)} -j ACCEPT',\n" +
+          "        apply_to => 'all',\n" +
+          "        order => 11\n" +
+          "      }\n" +
+          "      iptables::rule {'allow_decrypted_nc_traffic':\n" +
+          "        content => '-A LOCAL-INPUT -p tcp --dport #{nc_port} -j ACCEPT',\n" +
+          "        apply_to => 'all',\n" +
+          "        order => 11\n" +
+          "      }\n" +
+          "      iptables::listen::tcp_stateful { 'allow_sshd':\n" +
+          "        order => 8,\n" +
+          "        trusted_nets => ['ALL'],\n" +
+          "        dports => 22,\n" +
+          "      }\n"
+        }
 
-    #    it 'should apply manifest idempotently, restart ipsec service, start iptables with ipsec firewall' do
-    #      set_hieradata_on(left, hieradata_with_firewall_left)
-    #      set_hieradata_on(right,hieradata_with_firewall_right)
-    
+        it 'should apply manifest idempotently, restart ipsec service, start iptables with ipsec firewall' do
+          set_hieradata_on(left, hieradata_with_firewall_left)
+          set_hieradata_on(right,hieradata_with_firewall_right)
+
           # Apply ipsec and check for idempotency
-    #      apply_manifest_on(left, leftconnection_with_firewall, :catch_failures => true)
-    #      apply_manifest_on(right, rightconnection_with_firewall, :catch_failures => true)
-    #      apply_manifest_on(left, leftconnection_with_firewall, :catch_changes => true)
-    #      apply_manifest_on(right, rightconnection_with_firewall, :catch_changes => true)
+          apply_manifest_on(left, leftconnection_with_firewall, :catch_failures => true)
+          apply_manifest_on(right, rightconnection_with_firewall, :catch_failures => true)
+          apply_manifest_on(left, leftconnection_with_firewall, :catch_changes => true)
+          apply_manifest_on(right, rightconnection_with_firewall, :catch_changes => true)
 
-    #      [left, right].flatten.each do |node|
-    #        on node, "ipsec status", :acceptable_exit_codes => [0]
-    #        on node, 'iptables --list -v', :acceptable_exit_codes => [0] do
-    #          expect(stdout).to match(/ACCEPT\s+udp\s+--\s+any\s+any\s+#{client_net}\s+anywhere\s+state NEW multiport dports ipsec-nat-t,isakmp/m)
-    #          expect(stdout).to match(/ACCEPT\s+esp/m)
-    #          expect(stdout).to match(/ACCEPT\s+ah/m)
-    #        end
-    #      end
-    #    end
+          [left, right].flatten.each do |node|
+            on node, "ipsec status", :acceptable_exit_codes => [0]
+            on node, 'iptables --list -v', :acceptable_exit_codes => [0] do
+              expect(stdout).to match(/ACCEPT\s+udp\s+--\s+any\s+any\s+#{client_net}\s+anywhere\s+state NEW multiport dports ipsec-nat-t,isakmp/m)
+              expect(stdout).to match(/ACCEPT\s+esp/m)
+              expect(stdout).to match(/ACCEPT\s+ah/m)
+            end
+          end
+        end
 
-    #    it "should allow data carried by connection's tunnel" do
-    #      wait_for_command_success(left,  "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 1\"")
-    #      wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 1\"")
+        it "should allow data carried by connection's tunnel" do
+          wait_for_command_success(left,  "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 1\"")
+          wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 1\"")
 
           # send TCP data from right to left
-    #      on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
-    #      sleep(2)
-    #      on right, "echo 'this is a test of a tunnel with firewall enabled' | #{nc} -s #{rightip} #{left} #{nc_port} ", :acceptable_exit_codes => [0]
+          on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
+          sleep(2)
+          on right, "echo 'this is a test of a tunnel with firewall enabled' | #{nc} -s #{rightip} #{left} #{nc_port} ", :acceptable_exit_codes => [0]
 
           # verify data does reach left
-    #      on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
-    #        expect(stdout).to match(/this is a test of a tunnel with firewall enabled/)
-    #      end
+          on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
+            expect(stdout).to match(/this is a test of a tunnel with firewall enabled/)
+          end
 
           # verify iptables packet counts for esp and nc (over tcp) traffic have incremented
-    #      on left, 'iptables --list -v' do
-    #        expect(stdout).to match(/^(\s+[1-9]+[0-9]*\s+){2}ACCEPT\s+esp/m)
-    #        expect(stdout).to match(/^(\s+[1-9]+[0-9]*\s+){2}ACCEPT\s+tcp/m)
-    #      end
-    #    end
-    #  end
+          on left, 'iptables --list -v' do
+            expect(stdout).to match(/^(\s+[1-9]+[0-9]*\s+){2}ACCEPT\s+esp/m)
+            expect(stdout).to match(/^(\s+[1-9]+[0-9]*\s+){2}ACCEPT\s+tcp/m)
+          end
+        end
+      end
 
-    #  context 'when tunnel goes down with firewall protection' do
-    #    it 'should detect disabled tunnel' do
-    #      on left, 'puppet resource service ipsec ensure=stopped', :acceptable_exit_codes => [0]
-    #      wait_for_command_success(left, "ipsec status |& grep 'Pluto is not running'")
+      context 'when tunnel goes down with firewall protection' do
+        it 'should detect disabled tunnel' do
+          on left, 'puppet resource service ipsec ensure=stopped', :acceptable_exit_codes => [0]
+          wait_for_command_success(left, "ipsec status |& grep 'Pluto is not running'")
 
           # can take up to 2 minutes for right to timeout tunnel,
           # so restart instead to detect
           # failure immediately
-    #      wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 0\"")
-    #      wait_for_command_success(right, "ip xfrm policy | grep 'mode transport'")
-    #    end
+          wait_for_command_success(right, "ipsec status | egrep \"Total IPsec connections: loaded [1-9]+[0-9]*, active 0\"")
+          wait_for_command_success(right, "ip xfrm policy | grep 'mode transport'")
+        end
 
-    #    it "should drop data because of broken tunnel" do
+        it "should drop data because of broken tunnel" do
           # try to send TCP data from right to left
-    #      on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
-    #      sleep(2)
-    #      on right, "echo 'this is a test of a downed tunnel with firewall enabled' | #{nc} -s #{rightip} #{left} #{nc_port} ",
-    #        :acceptable_exit_codes => [1]
+          on left, "#{nc} -l #{nc_port} > #{testfile} &", :acceptable_exit_codes => [0]
+          sleep(2)
+          on right, "echo 'this is a test of a downed tunnel with firewall enabled' | #{nc} -s #{rightip} #{left} #{nc_port} ",
+            :acceptable_exit_codes => [1]
 
           # verify data does NOT reach left
-    #      on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
-    #        expect(stdout).to_not match(/this is a test of a downed tunnel with firewall enabled/)
-    #      end
-    #      on left, "pkill -f '#{nc} -l #{nc_port}'", :acceptable_exit_codes => [0]
-    #    end
-    #  end
+          on left, "cat #{testfile}", :acceptable_exit_codes => [0] do
+            expect(stdout).to_not match(/this is a test of a downed tunnel with firewall enabled/)
+          end
+          on left, "pkill -f '#{nc} -l #{nc_port}'", :acceptable_exit_codes => [0]
+        end
+      end
     end
     #TODO ipv6 tests
   end
