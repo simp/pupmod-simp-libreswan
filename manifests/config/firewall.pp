21ad690c1bc3dce6e521281a483248d7d40ea8fd
Adam Yohrling <ayohrling@users.noreply.github.com>
2020-08-06 09:15:11 -0400

    (SIMP-7458) Add EL8 Support (#50)
    This makes updates for support of EL8 IPsec connections. Adds support
    for firewalld in addition to iptables, removes old ciphers, and updates
    tests.
    
    Acceptance tests now support EL8 with firewalld and moves to using
    screen to background processes rather than forcing them to bg directly.
    This is in response to a yet-determined issue with net-ssh and OpenSSH
    on EL8 systems that hangs on return of backgrounding processes.
    
    SIMP-7458 #close

--- manifests/config/firewall.pp
+++ manifests/config/firewall.pp
@@ -3,28 +3,61 @@
 #
 class libreswan::config::firewall {
   assert_private()
-  iptables::listen::udp { 'ipsec_allow':
-    trusted_nets => $libreswan::trusted_nets,
-    apply_to     => 'all',
-    dports       => [ $::libreswan::ikeport, $::libreswan::nat_ikeport ],
-  }
 
-  # Add rules to allow the AH and ESP protocols used to encrypt data
-  iptables::rule { 'allow_protocol_esp':
-    content  => '-A LOCAL-INPUT -p esp  -j ACCEPT',
-    apply_to => 'all',
-    order    => 15,
-  }
+  $use_firewalld = simplib::lookup('iptables::use_firewalld', { 'default_value' => iptables::use_firewalld(true) })
 
-  iptables::rule { 'allow_protocol_ah_ipv4':
-    content  => '-A LOCAL-INPUT -p ah   -j ACCEPT',
-    apply_to => 'ipv4',
-    order    => 15,
-  }
+  if $use_firewalld {
+    simp_firewalld::rule { 'ipsec_allow':
+      trusted_nets => $::libreswan::trusted_nets,
+      apply_to     => 'all',
+      dports       => [
+        $::libreswan::ikeport,
+        $::libreswan::nat_ikeport,
+      ],
+      protocol     => 'udp',
+    }
 
-  iptables::rule { 'allow_protocol_ah_ipv6':
-    content  => '-A LOCAL-INPUT -m ah   -j ACCEPT',
-    apply_to => 'ipv6',
-    order    => 15,
+    simp_firewalld::rule { 'allow_protocol_esp':
+      trusted_nets => $::libreswan::trusted_nets,
+      apply_to     => 'all',
+      protocol     => 'esp',
+      order        => 15,
+    }
+
+    simp_firewalld::rule { 'allow_protocol_ah':
+      trusted_nets => $::libreswan::trusted_nets,
+      apply_to     => 'all',
+      protocol     => 'ah',
+      order        => 15,
+    }
+  }
+  else {
+    iptables::listen::udp { 'ipsec_allow':
+      trusted_nets => $libreswan::trusted_nets,
+      apply_to     => 'all',
+      dports       => [
+        $::libreswan::ikeport,
+        $::libreswan::nat_ikeport,
+      ],
+    }
+
+    # Add rules to allow the AH and ESP protocols used to encrypt data
+    iptables::rule { 'allow_protocol_esp':
+      content  => '-A LOCAL-INPUT -p esp  -j ACCEPT',
+      apply_to => 'all',
+      order    => 15,
+    }
+
+    iptables::rule { 'allow_protocol_ah_ipv4':
+      content  => '-A LOCAL-INPUT -p ah   -j ACCEPT',
+      apply_to => 'ipv4',
+      order    => 15,
+    }
+
+    iptables::rule { 'allow_protocol_ah_ipv6':
+      content  => '-A LOCAL-INPUT -m ah   -j ACCEPT',
+      apply_to => 'ipv6',
+      order    => 15,
+    }
   }
 }
